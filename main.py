617fc4c1a9a9a69ef2bea4029a3dc544578e6150
Raphaël Margueron <raphael.margueron@he-arc.ch>
2020-04-10 18:05:10 +0200

    auto format

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import os
 import requests
 import json
 
@@ -8,24 +7,31 @@ config = json.loads(open("config.ini", "r").read())
 
 youtubeEndpoint = "https://www.googleapis.com/youtube/v3/"
 
-playlistsEndPoint = youtubeEndpoint + "playlists?key={}&part=snippet&maxResults=50&channelId={}&pageToken={}"
-playlistItemsEndPoint = youtubeEndpoint + "playlistItems?key={}&part=status,snippet,contentDetails&maxResults=50&playlistId={}&pageToken={}"
+playlistsEndPoint = youtubeEndpoint + \
+    "playlists?key={}&part=snippet&maxResults=50&channelId={}&pageToken={}"
+playlistItemsEndPoint = youtubeEndpoint + \
+    "playlistItems?key={}&part=status,snippet,contentDetails&maxResults=50&playlistId={}&pageToken={}"
 channelEndPoint = youtubeEndpoint + "channels?key={}&part=id,snippet,statistics"
+
 
 def fetch_JSON(url):
     reply = requests.get(url)
     code = str(reply.status_code)
     content = json.loads(reply.content)
     if code[0] == "4" or code[0] == "5":
-        print("Error", str(content["error"]["code"]))
+        print(url)
+        print("Error : ", str(content["error"]["code"]))
         for er in content["error"]["errors"]:
+            print("Domain :", er["domain"])
             print("Reason :", er["reason"])
             print("Message :", er["message"])
         exit()
     return content
 
+
 def fetch_channel_data(usernameOrId):
-    url = channelEndPoint.format(config['apikey']) + "&forUsername=" + usernameOrId
+    url = channelEndPoint.format(
+        config['apikey']) + "&forUsername=" + usernameOrId
     channel = fetch_JSON(url)
     if len(channel['items']) > 0:
         return channel['items'][0]
@@ -37,11 +43,13 @@ def fetch_channel_data(usernameOrId):
         else:
             return None
 
+
 def fetch_playlists(channelId):
     playlistItems = []
     nextPageToken = ""
     while nextPageToken != None:
-        url = playlistsEndPoint.format(config['apikey'], channelId, nextPageToken)
+        url = playlistsEndPoint.format(
+            config['apikey'], channelId, nextPageToken)
         playlists = fetch_JSON(url)
         try:
             if 'nextPageToken' in playlists:
@@ -53,11 +61,13 @@ def fetch_playlists(channelId):
             print(playlists)
     return playlistItems
 
+
 def fetch_playlist_content(playlistId):
     videosItems = []
     nextPageToken = ""
-    while nextPageToken != None:
-        url = playlistItemsEndPoint.format(config['apikey'], playlistId, nextPageToken)
+    while nextPageToken is not None:
+        url = playlistItemsEndPoint.format(
+            config['apikey'], playlistId, nextPageToken)
         videos = fetch_JSON(url)
         try:
             if 'nextPageToken' in videos:
@@ -69,36 +79,42 @@ def fetch_playlist_content(playlistId):
             print(videos)
     return videosItems
 
+
 def channel_infos(channel):
     print("-------------------------------")
-    print("Title : " + channel['snippet']['title'])
-    print("Description : " + channel['snippet']['description'])
-    print("Url : https://www.youtube.com/channel/" + channel['id'])
+    print(f"Title : {channel['snippet']['title']}")
+    print(f"Description : {channel['snippet']['description']}")
+    print(f"Url : https://www.youtube.com/channel/{channel['id']}")
     if "customUrl" in channel['snippet']:
-        print("Custom Url : https://www.youtube.com/" + channel['snippet']['customUrl'])
-    print("Creation : " + channel['snippet']['publishedAt'])
+        print(
+            f"Custom Url : https://www.youtube.com/{channel['snippet']['customUrl']}")
+    print(f"Creation : {channel['snippet']['publishedAt']}")
     if "county" in channel['snippet']:
-        print("Country : " + channel['snippet']['country'])
-    print("Number of subscribers : " + channel['statistics']['subscriberCount'])
-    print("Number of views : " + channel['statistics']['viewCount'])
-    print("Number of videos : " + channel['statistics']['videoCount'])
+        print(f"Country : {channel['snippet']['country']}")
+    print(
+        f"Number of subscribers : {channel['statistics']['subscriberCount']}")
+    print(f"Number of views : {channel['statistics']['viewCount']}")
+    print(f"Number of videos : {channel['statistics']['videoCount']}")
     print("-------------------------------")
 
+
 def video_infos(video):
-    return "[" + video['status']['privacyStatus'] + "] : " + video['snippet']['title'] + " (https://youtu.be/"+ video['contentDetails']['videoId'] +")"
+    return f"[{video['status']['privacyStatus']}] : {video['snippet']['title']} (https://youtu.be/{video['contentDetails']['videoId']})"
+
 
 def playlist_infos(playlist):
-    return playlist['snippet']['title'] + " (https://www.youtube.com/playlist?list="+ playlist['id'] + ")"
+    return f"{playlist['snippet']['title']} (https://www.youtube.com/playlist?list={playlist['id']})"
+
 
 def analyse():
     notPublicVideos = []
 
     channel = None
 
-    while channel == None:
+    while channel is None:
         username = input("Username/Channel ID : ")
         channel_data = fetch_channel_data(username)
-        if channel_data == None:
+        if channel_data is None:
             print("Invalid channel")
             continue
 
@@ -135,6 +151,7 @@ def analyse():
         print("No videos found !")
     for playlist, video in notPublicVideos:
         print(video_infos(video) + " in " + playlist_infos(playlist))
+
 
 if __name__ == '__main__':
     while True:
